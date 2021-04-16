3cc4deb3596b46492756c555491383702ab26ad6
David Guida <david.guida.03@gmail.com>
2021-04-16 18:43:21 -0400

    updated destination path

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,6 +1,7 @@
 solutionName="Chip8Emulator"
 projectName="Chip8Emulator.BlazorWasm"
 projectFile="$projectName/$projectName.csproj"
+destFolderName="chip8-emulator"
 
 rm -rf ./build
 
@@ -11,4 +12,4 @@ dotnet publish $projectFile --output $buildPath --configuration Release
 mv -v $buildPath/wwwroot/* $buildPath/wwwroot/.* ./build      
 rm -rf $buildPath
 
-sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/$solutionName\/\" \/>/g" ./build/index.html
+sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/$destFolderName\/\" \/>/g" ./build/index.html
