06cb5fa5c585a03b8938630815b4dcc3614b4801
David Guida <david.guida.03@gmail.com>
2021-04-16 09:52:21 -0400

    first commit

---
+++ Chip8Emulator.Core/LambdaRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Chip8Emulator.Core
+{
+    public class LambdaRenderer : IRenderer
+    {
+        private readonly Action<bool[,]> _update;
+        private readonly Action _render;
+
+        public LambdaRenderer(Action<bool[,]> update, Action render)
+        {
+            _update = update;
+            _render = render;
+        }
+
+        public void Render()
+            => _render?.Invoke();
+
+        public void Update(bool[,] screen)
+            =>_update?.Invoke(screen);        
+    }
+}
