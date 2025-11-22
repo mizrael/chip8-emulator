d6d4ef0e955486410b44f1c299df8b955a2e0569
David Guida <1432872+mizrael@users.noreply.github.com>
2025-11-22 14:02:32 -0500

    added sound player

---
+++ Chip8Emulator.BlazorWasm/BlazorSoundPlayer.cs
@@ -0,0 +1,17 @@
+﻿using Chip8Emulator.Core;
+using Microsoft.JSInterop;
+
+namespace Chip8Emulator.BlazorWasm;
+
+public class BlazorSoundPlayer : ISoundPlayer
+{
+    private readonly IJSRuntime _js;
+    public BlazorSoundPlayer(IJSRuntime js) => _js = js;
+
+    public void Beep(int milliseconds)
+    {
+        if (milliseconds <= 0) return;
+
+        _ = _js.InvokeVoidAsync("chip8Sound.beep", 500, milliseconds);
+    }
+}
---
+++ Chip8Emulator.BlazorWasm/wwwroot/soundPlayer.js
@@ -0,0 +1,24 @@
+﻿window.chip8Sound = {
+    ctx: null,
+    ensure: function () {
+        if (!this.ctx) this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+    },
+    beep: function (freq, ms) {
+        this.ensure();
+        const ctx = this.ctx;
+        const osc = ctx.createOscillator();
+        const gain = ctx.createGain();
+        osc.type = 'square';
+        osc.frequency.value = freq;
+        osc.connect(gain);
+        gain.connect(ctx.destination);
+
+        const now = ctx.currentTime;
+        const duration = ms / 1000.0;
+        gain.gain.setValueAtTime(0.2, now);
+        gain.gain.exponentialRampToValueAtTime(0.0001, now + duration);
+
+        osc.start(now);
+        osc.stop(now + duration);
+    }
+};
